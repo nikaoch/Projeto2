dfecaa3cbc7acf80584185836930a61da9d8224f
Nicolas Souza <nj776640@gmail.com>
2025-02-26 12:31:30 -0300

    modificações nos arquivos

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const form = document.getElementById('form-atividade');
 const imgAprovado = '<img src="./Imagens/aprovado.png" alt="emoji festejando"></td>';
 const imgReprovado = '<img src="./Imagens/reprovado.png" alt="emoji cabisbaixo"></td>';
 let linhas = ''; //variáveis fora do eventlistener para q n sejam resetadas pelo submit, e apareçam como um histórico
-
+const atividades = [];
+const notas = [];
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -14,6 +15,9 @@ form.addEventListener('submit', function(e) {
     function adicionaLinha() {
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
 
     let linha = '<tr>';
     linha += `<td>${inputNomeAtividade.value}</td>`;
@@ -30,4 +34,21 @@ form.addEventListener('submit', function(e) {
     function atualizaTabela() {
     const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML = linhas;
+}
+
+function atualizaMediaFinal() {
+    const mediaFinal = calculaMediaFinal();
+
+    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? 'Aprovado' : 'Reprovado';
+}
+
+function calculaMediaFinal() {
+    let somaDasNotas = 0;
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i];
+    }
+
+    return somaDasNotas / notas.length;
 }
