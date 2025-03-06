5074407e027b183ce751c034fcd0fd4e083cd552
Nicolas Souza <nj776640@gmail.com>
2025-03-06 01:18:05 -0300

    removido resultados fixados no tfoot, acrescimo da verificação de matérias repetidas e confirmação de nota minima

--- main.js
+++ main.js
@@ -4,28 +4,37 @@ const imgReprovado = '<img src="./Imagens/reprovado.png" alt="emoji cabisbaixo">
 let linhas = ''; //variáveis fora do eventlistener para q n sejam resetadas pelo submit, e apareçam como um histórico
 const atividades = [];
 const notas = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
+const notaMinima = parseFloat(prompt("Digite a nota mínima:"));
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     adicionaLinha();
-    atualizaTabela();
-})
+    atualizaTabela(); //funções q seram executadas qd o botão submit for acionado
+    atualizaMediaFinal();
+});
 
     function adicionaLinha() {
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
-    atividades.push(inputNomeAtividade.value);
-    notas.push(parseFloat(inputNotaAtividade.value));
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert(`A atividade: ${inputNomeAtividade.value} já foi inserida!`);
+    } else { 
+        atividades.push(inputNomeAtividade.value);
+        notas.push(parseFloat(inputNotaAtividade.value));
 
-    let linha = '<tr>';
-    linha += `<td>${inputNomeAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
-    linha += '</tr>';
+        let linha = '<tr>';
+        linha += `<td>${inputNomeAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`;
+        linha += '</tr>';
 
-    linhas += linha;
+        linhas += linha;
+    }
+
 
     inputNomeAtividade.value = '';
     inputNotaAtividade.value = '';
@@ -40,7 +49,7 @@ function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
     document.getElementById('media-final-valor').innerHTML = mediaFinal;
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? 'Aprovado' : 'Reprovado';
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
 function calculaMediaFinal() {
@@ -50,5 +59,5 @@ function calculaMediaFinal() {
         somaDasNotas += notas[i];
     }
 
-    return somaDasNotas/notas.length;
+    return somaDasNotas / notas.length;
 }
